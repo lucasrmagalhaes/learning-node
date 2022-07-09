bcc4081b5569c50ab65fbc6c1c117ffb8c41f621
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-07-09 11:11:47 -0300

    Create a get messages service

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-var express, app, server;
+var express, app, server, messages;
 
 express = require('express')
 
 app = express()
 app.use(express.static(__dirname))
+
+messages = [
+    { name: 'Lucas', message: 'Hi' },
+    { name: 'Tim', message: 'Hello' },
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
 
 server = app.listen(3000, () => {
     console.log('Server is listening on port', server.address().port)
