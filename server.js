27ec1bfcddf140700225fcf8b85062df0573a250
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-07-09 23:43:17 -0300

    Finally

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ app.post('/messages', async (req, res) => {
     } catch (error) {
         res.sendStatus(500)
         return console.error(error)
+    } finally {
+        console.log('Message post called')
     }
 })
 
